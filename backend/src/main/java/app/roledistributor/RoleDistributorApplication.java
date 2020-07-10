b2d14f69e5e9c55b7df116b662fd40577fe0079d
kuenhwi <kueniyam@naver.com>
2020-07-10 14:33:14 +0900

    init: backend

---
+++ backend/src/main/java/app/roledistributor/RoleDistributorApplication.java
@@ -0,0 +1,13 @@
+package app.roledistributor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleDistributorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleDistributorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/app/roledistributor/RoleDistributorApplicationTests.java
@@ -0,0 +1,13 @@
+package app.roledistributor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleDistributorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
