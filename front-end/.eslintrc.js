f0d2ddbe5ab34fe61e4ed5d45391643cc42ee039
kuenhwi <kueniyam@naver.com>
2020-07-10 15:10:12 +0900

    chore: prettier 생성 및 적용

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
         "es2020": true
     },
     "extends": [
+        "plugin:prettier/recommended",
         "eslint:recommended",
         "plugin:react/recommended"
     ],
